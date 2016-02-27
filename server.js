8b6ddb4354c4bae248a9049f534bc952325fff26
robjloranger <robjloranger@gmail.com>
2016-02-27 10:09:27 -0800

    fixed port typo

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var PORT = process.argv.PORT || 8080;
+var PORT = process.env.PORT || 8080;
 app.get('/', function(req, res){
   var userAgent = req.headers['user-agent'];
   var headerInfo = {
