9dc1e46a4d33b7a67850e3511bb85ea7a1f9aecf
robjloranger <robjloranger@gmail.com>
2016-02-27 10:13:16 -0800

    removed excess leading on ip string

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var PORT = process.env.PORT || 8080;
 app.get('/', function(req, res){
   var userAgent = req.headers['user-agent'];
   var headerInfo = {
-    ip: req.ip,
+    ip: req.ip.slice(req.ip.lastIndexOf(':')+1,req.ip.length),
     language: req.get("Accept-Language").slice(0,5),
     'operating system': userAgent.slice(userAgent.indexOf('(')+1,userAgent.indexOf(')'))
   };
