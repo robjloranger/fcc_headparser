8f7871ff9ee0a35698bc874c3574f1a54583f76e
robjloranger <robjloranger@gmail.com>
2016-02-27 14:06:22 -0800

    hopefully solution to being behind heroku

--- server.js
+++ server.js
@@ -3,8 +3,15 @@ var app = express();
 var PORT = process.env.PORT || 8080;
 app.get('/', function(req, res){
   var userAgent = req.headers['user-agent'];
+  var ipAddr = req.headers['x-forwarded-for'];
+  if(ipAddr){
+    var addss = ipAddr.split(',');
+    ipAddr = addss[addss.length-1];
+  }else{
+    ipAddr = req.ip.slice(req.ip.lastIndexOf(':')+1,req.ip.length);
+  }
   var headerInfo = {
-    ip: req.ip.slice(req.ip.lastIndexOf(':')+1,req.ip.length),
+    ip: ipAddr,
     language: req.get("Accept-Language").slice(0,5),
     'operating system': userAgent.slice(userAgent.indexOf('(')+1,userAgent.indexOf(')'))
   };
